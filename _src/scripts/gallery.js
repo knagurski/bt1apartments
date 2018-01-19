382e2bc833c42d7727daf0abad7701162e96d099
Kevin Nagurski <knagurski@gmail.com>
2018-01-19 23:58:40 +0000

    Fix floating heading

--- _src/scripts/gallery.js
+++ _src/scripts/gallery.js
@@ -1,6 +1,6 @@
 if (window.jQuery) {
-    var $header = $('body > header');
+  var $header = $('main > header')
 
-    $header.next().css({marginTop: $header.height()});
-    $header.addClass('fixed');
+  $header.next().css({marginTop: $header.height()})
+  $header.addClass('fixed')
 }
