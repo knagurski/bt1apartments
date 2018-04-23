12808a7880fc8a555fe0fa77214dc2622fbe273c
Kevin Nagurski <knagurski@gmail.com>
2018-04-23 22:47:39 +0100

    Fix bug fired when productions gallery not present

--- _src/scripts/productions-gallery.js
+++ _src/scripts/productions-gallery.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const productionsGallery = document.querySelector('.productions__gallery');
 
-  if (window.IntersectionObserver) {
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.filter(entry => entry.isIntersecting).forEach(({target}) => {
-        observer.unobserve(target)
+  if (productionsGallery) {
+    if (window.IntersectionObserver) {
+      const observer = new IntersectionObserver((entries, observer) => {
+        entries.filter(entry => entry.isIntersecting).forEach(({target}) => {
+          observer.unobserve(target)
 
-        loadGallery()
-      })
-    }, {rootMargin: '30px'})
+          loadGallery()
+        })
+      }, {rootMargin: '30px'})
 
-    observer.observe(productionsGallery)
-  } else {
-    loadGallery()
+      observer.observe(productionsGallery)
+    } else {
+      loadGallery()
+    }
   }
 
   function loadGallery () {
